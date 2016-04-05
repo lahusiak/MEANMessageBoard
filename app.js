2ab9ab14bdb600054019625fa99a79fe0f0699b7
lahusiak <lahusiak@gmail.com>
2016-04-05 18:06:21 -0500

    Implemented angular routing

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: "PostsCtrl"
+      });
+      $urlRouterProvider.otherwise('home');
+  }]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+
+
+// CONTROLLERS
 app.controller('MainCtrl', [
-'$scope',
-  function($scope){
+  '$scope',
+  'posts',
+  function($scope, posts){
     $scope.posts = [
       {title: 'post 1', upvotes: 5},
       {title: 'post 2', upvotes: 2},
@@ -15,15 +45,41 @@ app.controller('MainCtrl', [
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0});
+        upvotes: 0,
+        comments:[
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Mary', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
       $scope.title = '';
       $scope.link = '';
     };
     $scope.incrementUpvotes = function(post){
       post.upvotes += 1;
-    }
+    };
+    $scope.posts = posts.posts;
   }]);
 
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if($scope.body === ''){return;}
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+
+      $scope.body = '';
+    }
+  }
+
+
+]);
 //Node.js was installed at
 //
 //    /usr/local/bin/node
